771af2f24ded26fa594288c2f725457e9568ed52
builder-main <32265170+builder-main@users.noreply.github.com>
2023-03-22 11:04:17 +0100

    Update args-Unity-BlenderToFBX.py

--- args-Unity-BlenderToFBX.py
+++ args-Unity-BlenderToFBX.py
@@ -2,17 +2,23 @@ import bpy
 import bmesh
 blender249 = True
 blender280 = (2,80,0) <= bpy.app.version
-import sys
 
 #
 # FUNCTION TO APPLY ALL SHARED MODIFIERS
 #
 def ApplySharedModifiers():
-    #start by OBJECT mode and deselect all
+    
+    #start by OBJECT mode 
+    bpy.ops.object.mode_set(mode='OBJECT')
+
+    collections = bpy.data.collections
+    cLen = len(collections)    
     objs = bpy.data.objects
+    #objs = bpy.data.collections[0].all_objects
     oLen = len(objs)
+    print("Will look for shared modifiers in "+str(objs)+" objects and "+str(cLen)+" collections")
     #1. Group by shared mesh data
-    groups = {}
+    meshDataList = {}
     for obj in objs:
 
         if obj.data is None:
@@ -21,82 +27,104 @@ def ApplySharedModifiers():
 
         meshDataName = obj.data.name
 
-        if not groups.get(meshDataName):
-            groups[meshDataName] = [] 
+        if not meshDataList.get(meshDataName):
+            meshDataList[meshDataName] = [] 
             print("Shared Mesh "+meshDataName)
             
-        groups[meshDataName].append(obj)
+        meshDataList[meshDataName].append(obj)
         print("\tAdd object"+obj.name)
         
-    #2. Keep groups with shared only data and modifiers.
-    for groupName in groups:
+    #2. Keep meshDataList with shared only data and modifiers.
+    for meshDataName in meshDataList:
         # check same modifiers
-        group = groups[groupName]
-        mainObject = group[0]
-        modifiers = mainObject.modifiers
-        print("Checking shared "+groupName)
+        meshDataRef = meshDataList[meshDataName]
+        meshData = meshDataRef[0]
+        modifiers = meshData.modifiers
+        
+        #meshDataName are kept in blend files even if no object are referencing them. Check that before trying to select them.
+        isInView = False
+        for view in bpy.context.view_layer.objects:
+            isInView |= view.name == meshData.name
+
+        if not isInView:
+            print("Ignore Mesh Data as not attached to any object in view : "+meshData.name)
+            continue        
+        
+        print("Checking mesh data "+meshDataName)
+                
         groupValid = ''
         for modifier in modifiers:
             print("\tChecking modifier "+modifier.name)
-            for obj in group[1:]:
+            for obj in meshDataRef[1:]:
               if obj.modifiers.find(modifier.name) == -1:
                   groupValid = obj.name+"."+modifier.name
                   break
+        
         if not groupValid == '': 
             print("\tNot Shared "+groupValid)
             continue       
-             
+
+        #If there is no shared modifiers on same named mesh we should return to avoid blender import issues.     
         if len(modifiers) == 0:
-            print("\tNo modifier on "+mainObject.name)
+            print("\tNo modifier on "+meshData.name)
             continue   
-              
-        #3. Apply to geometry modifiers on first group data    
-        bpy.context.view_layer.objects.active = mainObject
-        mainObject.select_set(state=True)
+
+    
+
+        #3. Apply to geometry modifiers on first meshData data    
+        bpy.context.view_layer.objects.active = meshData
+        meshData.select_set(state=True)
         bpy.ops.object.mode_set(mode='OBJECT')
         #low level convert
         depGraph = bpy.context.evaluated_depsgraph_get()        
         bm = bmesh.new()
-        bm.from_object(mainObject, depGraph)
-        bm.to_mesh(mainObject.data)
+        bm.from_object(meshData, depGraph)
+        bm.to_mesh(meshData.data)
         
-        mainObject.select_set(state=False)
+        meshData.select_set(state=False)
         
         #4. clean modifiers for other
-        for obj in group:
+        for obj in meshDataRef:
             #print("\tobj.modifiers.clear()")
             obj.modifiers.clear()
 
-	
-try: import Blender
+
+
+try:
+    import Blender
 except:
-	blender249 = False
-	import bpy	
+    blender249 = False
 
 if not blender280:
     if blender249:
-        try: import export_fbx
+        try:
+            import export_fbx
         except:
             print('error: export_fbx not found.')
             Blender.Quit()
-    else:
-        try: import io_scene_fbx.export_fbx
+    else :
+        try:
+            import io_scene_fbx.export_fbx
         except:
             print('error: io_scene_fbx.export_fbx not found.')
             # This might need to be bpy.Quit()
             raise
 
-# Accept command line arguments and load the contents of the .blend file
-infile = sys.argv[5]
-bpy.ops.wm.open_mainfile(filepath=infile)
-outfile = sys.argv[6]
-
+# Find the Blender output file
+import os
+outfile = os.getenv("UNITY_BLENDER_EXPORTER_OUTPUT_FILE")
+if not outfile:
+    outfile = "./DebugLocal.fbx"
 
 # Do the conversion
 print("Starting blender to FBX conversion " + outfile)
 
-#APPLY ALL SHARED MODIFIERS
-ApplySharedModifiers()
+#TRY APPLY ALL SHARED MODIFIERS
+try:
+    ApplySharedModifiers()
+except exception:
+    print("Couldn't apply modifiers, see exception below. Will continue with default behaviour") 
+    print(exception) 
 
 if blender280:
     import bpy.ops
@@ -108,58 +136,56 @@ if blender280:
         use_mesh_modifiers=True,
         mesh_smooth_type='OFF',
         use_custom_props=True,
-        bake_anim_use_nla_strips=False,
-        bake_anim_use_all_actions=False,
+        bake_anim_use_nla_strips=True,
+        bake_anim_use_all_actions=True,
         apply_scale_options='FBX_SCALE_ALL')
 elif blender249:
-	mtx4_x90n = Blender.Mathutils.RotationMatrix(-90, 4, 'x')
-	export_fbx.write(outfile,
-		EXP_OBS_SELECTED=False,
-		EXP_MESH=True,
-		EXP_MESH_APPLY_MOD=True,
-		EXP_MESH_HQ_NORMALS=True,
-		EXP_ARMATURE=True,
-		EXP_LAMP=True,
-		EXP_CAMERA=True,
-		EXP_EMPTY=True,
-		EXP_IMAGE_COPY=False,
-		ANIM_ENABLE=True,
-		ANIM_OPTIMIZE=False,
-		ANIM_ACTION_ALL=True,
-		GLOBAL_MATRIX=mtx4_x90n)
+    mtx4_x90n = Blender.Mathutils.RotationMatrix(-90, 4, 'x')
+    export_fbx.write(outfile,
+        EXP_OBS_SELECTED=False,
+        EXP_MESH=True,
+        EXP_MESH_APPLY_MOD=True,
+        EXP_MESH_HQ_NORMALS=True,
+        EXP_ARMATURE=True,
+        EXP_LAMP=True,
+        EXP_CAMERA=True,
+        EXP_EMPTY=True,
+        EXP_IMAGE_COPY=False,
+        ANIM_ENABLE=True,
+        ANIM_OPTIMIZE=False,
+        ANIM_ACTION_ALL=True,
+        GLOBAL_MATRIX=mtx4_x90n)
 else:
-	# blender 2.58 or newer
-	import math
-	from mathutils import Matrix
-	# -90 degrees
-	mtx4_x90n = Matrix.Rotation(-math.pi / 2.0, 4, 'X')
-	
-	print("moo")
-	
-	class FakeOp:
-		def report(self, tp, msg):
-			print("%s: %s" % (tp, msg))
-	
-	exportObjects = ['ARMATURE', 'EMPTY', 'MESH']
-		
-	minorVersion = bpy.app.version[1];	
-	if minorVersion <= 58:
-		# 2.58
-		io_scene_fbx.export_fbx.save(FakeOp(), bpy.context, filepath=outfile,
-			global_matrix=mtx4_x90n,
-			use_selection=False,
-			object_types=exportObjects,
-			mesh_apply_modifiers=True,
-			ANIM_ENABLE=True,
-			ANIM_OPTIMIZE=False,
-			ANIM_OPTIMIZE_PRECISSION=6,
-			ANIM_ACTION_ALL=True,
-			batch_mode='OFF',	
-			BATCH_OWN_DIR=False)
-	else:
-		# 2.59 and later
-		kwargs = io_scene_fbx.export_fbx.defaults_unity3d()
-		io_scene_fbx.export_fbx.save(FakeOp(), bpy.context, filepath=outfile, **kwargs)
-	# HQ normals are not supported in the current exporter
+    # blender 2.58 or newer
+    import math
+    from mathutils import Matrix
+    # -90 degrees
+    mtx4_x90n = Matrix.Rotation(-math.pi / 2.0, 4, 'X')
+
+    class FakeOp:
+        def report(self, tp, msg):
+            print("%s: %s" % (tp, msg))
+
+    exportObjects = ['ARMATURE', 'EMPTY', 'MESH']
+
+    minorVersion = bpy.app.version[1];
+    if minorVersion <= 58:
+        # 2.58
+        io_scene_fbx.export_fbx.save(FakeOp(), bpy.context, filepath=outfile,
+            global_matrix=mtx4_x90n,
+            use_selection=False,
+            object_types=exportObjects,
+            mesh_apply_modifiers=True,
+            ANIM_ENABLE=True,
+            ANIM_OPTIMIZE=False,
+            ANIM_OPTIMIZE_PRECISSION=6,
+            ANIM_ACTION_ALL=True,
+            batch_mode='OFF',
+            BATCH_OWN_DIR=False)
+    else:
+        # 2.59 and later
+        kwargs = io_scene_fbx.export_fbx.defaults_unity3d()
+        io_scene_fbx.export_fbx.save(FakeOp(), bpy.context, filepath=outfile, **kwargs)
+    # HQ normals are not supported in the current exporter
 
 print("Finished blender to FBX conversion " + outfile)
